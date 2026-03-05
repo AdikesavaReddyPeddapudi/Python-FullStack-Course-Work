5baf0bfabec360e86706e119b248e83ed47dbc7e
Adikesava Reddy Peddapudi <121013102+AdikesavaReddyPeddapudi@users.noreply.github.com>
2026-03-05 21:12:32 +0530

    Add files via upload

---
+++ Day-2/personal_info.py
@@ -0,0 +1,62 @@
+Student_Name = 'Adikesava Reddy Peddapudi'
+Student_ID = 'CGH2900'
+Batch_No = 'PFS-HYD-050'
+Email_ID = 'adikesavareddypeddapudi@gmail.com'
+Date_of_Birth = '2003-02-09'
+Age = 23
+Gender = 'Male'
+Blood_Group = 'N/A'
+City = 'Hyderabad'
+State = 'Telengana'
+Student_Phone_Number = 9866482585
+Parent_Phone_Number = 9347496292
+Github_Link = 'https://github.com/AdikesavaReddyPeddapudi'
+
+College_Name = 'Seshadri Rao Gudlavalleru Engineering college'
+USN_Number = '21481A12B8'
+Qualification = 'UG (Bachelor Degrees)'
+Department = 'B.Tech - Information Technology (IT)'
+Pass_out_Year = 2025
+Graduation_Percentage = '80%'
+Arrears = 'No'
+No_of_Arrears = 0
+
+tenth_Pass_Year = 2019
+tenth_Percentage = '98%'
+
+twelfth_Pass_Year = 2021
+twelfth_Percentage = '95%'
+
+Skills = 'Python, Flask, HTML, CSS, Bootstrap, Javascript, MySQL'
+
+print("Student Name:", Student_Name)
+print("Student ID:", Student_ID)
+print("Batch No:", Batch_No)
+print("Email:", Email_ID)
+print("Date of Birth:", Date_of_Birth)
+print("Age:", Age)
+print("Gender:", Gender)
+print("Blood Group:", Blood_Group)
+print("City:", City)
+print("State:", State)
+print("Student Phone:", Student_Phone_Number)
+print("Parent Phone:", Parent_Phone_Number)
+print("Github Link:", Github_Link)
+
+print("\n--- Academic Information ---")
+print("College Name:", College_Name)
+print("USN Number:", USN_Number)
+print("Qualification:", Qualification)
+print("Department:", Department)
+print("Passout Year:", Pass_out_Year)
+print("Graduation Percentage:", Graduation_Percentage)
+print("Arrears:", Arrears)
+print("No of Arrears:", No_of_Arrears)
+
+print("\n--- School Education ---")
+print("10th Pass Year:", tenth_Pass_Year)
+print("10th Percentage:", tenth_Percentage)
+print("12th Pass Year:", twelfth_Pass_Year)
+print("12th Percentage:", twelfth_Percentage)
+
+print("\nSkills:", Skills)
